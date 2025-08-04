f95f137d372a6e72cf5e0cc58245c8c2a9ef510f
yelizesgi <yelizesgi2014@gmail.com>
2025-08-04 23:03:45 +0300

    Node.Js start

---
+++ SQL/03-SQL_3/04-Nodejs/index.js
@@ -0,0 +1,20 @@
+/* ------------------- NODEJS --------------------- */
+
+// Official: https://nodejs.org/
+// Documents: https://nodejs.org/api
+
+
+/* ------------------- Node Shell --------------------- */
+
+// $ node - nodejs ortamina terminalden ulsabilirim
+// Her hangi bir js kodunu veya bir pateketi indirebilirm
+// .exit - nodejs runtime ortamindan cikmak icin kullanilir
+
+
+
+/* ------------------- Index.js --------------------- */
+// to run index.js file : node index.js
+// npm i --save-dev nodemon
+
+// const message = 'hi CH19!'
+// console.log(message);
