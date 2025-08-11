992f3d5279cf8ab99fda970dc62a86fb52eb439b
yelizesgi <yelizesgi2014@gmail.com>
2025-08-11 22:35:59 +0300

    oop-ws update inheritance

--- JS-OOP/oop-ws.js
+++ JS-OOP/oop-ws.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 class Clothes {
   option = true;
   constructor(model, color, genre, price) {
@@ -15,9 +15,43 @@ class Clothes {
 }
 
 /* ------------------------------------------------------- *
-//? Instance * Bir class'tan obje Türetme denemesi
+//? Instance * Bir class'tan obje Türetme denemesi//
 
 const MyClothes = new Clothes("Shirt", "Green", "Male", "100$");
 console.log(MyClothes);
 console.log(MyClothes.getDetails());
+/* ------------------------------------------------------- */
+//? INHERITANCE: Miras Alma. Başka bir class'ın tüm özeelik/metodlarını devralma. (parent-child ilişkisi kurulur.)
+//? SUPER: Parent (Üst) Class - THIS: Child (Alt) Class
+
+class Wear {
+  sectionİsActive = true;
+
+  constructor(wearType) {
+    this.wearType = wearType;
+  }
+}
+
+class Clothes extends Wear {
+  //Inheritance
+  option = true;
+  constructor(model, color, genre, price, wearType) {
+    super(wearType);
+    this.model = model;
+    this.color = color;
+    this.genre = genre;
+    this.price = price;
+  }
+
+  selectionMade(param1) {
+    this.option = false;
+    return "The Men clotheses made";
+  }
+}
+
+const WomenClothes = new Clothes("Dress", "Pink", "women", "120$");
+console.log(WomenClothes);
+
+/* ------------------------------------------------------- */
+
 /* ------------------------------------------------------- */
