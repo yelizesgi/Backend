9a115b779b50fc4bf7fb2ce0da5f1122a73ee7e3
yelizesgi <yelizesgi2014@gmail.com>
2025-08-21 22:21:18 +0300

    Routes-Middleware WS

---
+++ Workshop/Routes_Middleware/routes/index.js
@@ -0,0 +1,22 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - ROUTING
+------------------------------------------------------- */
+
+// const express = require('express')
+const router = require('express').Router()
+
+// URL: /test ->
+router.get('/', (req, res) => { res.send({ message: 'Home Page' }) })
+router.get('/path', (req, res) => { res.send({ message: 'Path Page' }) })
+router.post('/', (req, res) => { res.send({ message: 'Post Page' }) })
+router.put('/', (req, res) => { res.send({ message: 'Put Page' }) })
+router.delete('/', (req, res) => { res.send({ message: 'Delete Page' }) })
+
+// router.route('/')
+//     .get((req, res) => { res.send('get')})
+//     .post((req, res) => { res.send('post')})
+
+router.get('/user/:id', (req, res) => { res.send({ message: req.params.id }) })
+
+module.exports = router
