73e0699fb475ff0142c2798a4b4079d0fe5c9721
yelizesgi <yelizesgi2014@gmail.com>
2025-09-24 23:03:21 +0300

    25-InClass updated

--- 24-StockApi/src/middlewares/authentication.js
+++ 24-StockApi/src/middlewares/authentication.js
@@ -1,10 +1,32 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 // app.use(authentication):
+const Token = require("../models/token");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
+  req.user = null;
 
-    next()
-}
+  const auth = req.headers?.authorization || null;
+  const tokenArr = auth ? auth.split(" ") : null;
+
+  if (tokenArr) {
+    if (tokenArr[0] == "Token") {
+      const { userId } = await Token.findOne({ token: tokenArr[1] }).populate(
+        "userId"
+      );
+      req.user = userId ? userId : null;
+      
+    } else if (tokenArr[0] == "Bearer") {
+      jwt.verify(
+        tokenArr[1],
+        process.env.ACCESS_KEY,
+        (err, userData) => (req.user = userData ? userData : null)
+      );
+    }
+  }
+
+  next();
+};
